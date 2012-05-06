eefb10ae65bf1e6b74328a36f70f5bc02d9910f3
Oliver Wegner <void1976@gmail.com>
2012-05-06 17:52:06 +0200

    added scala nature to project, yeah!

---
+++ gojabz-appengine/src/com/gojabz/server/OwTestScalaService.scala
@@ -0,0 +1,13 @@
+package com.gojabz.server
+
+import javax.servlet.http.HttpServlet
+import javax.servlet.http.HttpServletRequest
+import javax.servlet.http.HttpServletResponse
+
+class OwTestScalaService extends HttpServlet {
+
+  override def doGet(request: HttpServletRequest, response: HttpServletResponse) = {
+	  println( "called scala doGet()!" )
+	  response.getOutputStream.print("called scala doGet()")
+  }
+}
